b111792718e6768571988456adf5a62e83b879b5
johnj974 <58571462+johnj974@users.noreply.github.com>
2021-01-05 18:19:29 +0000

    teams+landing components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing/landing.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LandingComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule

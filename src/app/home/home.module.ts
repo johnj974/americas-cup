5ddfdc3523457726b50c544978f09e7984d3abba
johnj974 <58571462+johnj974@users.noreply.github.com>
2021-01-19 19:59:17 +0000

    added better styling to elements next to header

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingComponent } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingComponent],
+  imports: [CommonModule, HomeRoutingComponent, SharedModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}

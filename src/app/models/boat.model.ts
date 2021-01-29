13820c97d426a4c00b3d13f0be8e31374c585eb3
johnj974 <jjdug@hotmail.com>
2021-01-29 19:52:52 +0000

    added official name property

--- src/app/models/boat.model.ts
+++ src/app/models/boat.model.ts
@@ -2,6 +2,7 @@ export class Boat {
   public id: string;
   public country: string;
   public name: string;
+  public officialName: string;
   public skipper: string;
   public helmsman: string;
   public sponsors: string;
@@ -12,15 +13,17 @@ export class Boat {
     id: string,
     country: string,
     name: string,
+    officialName: string,
     skipper: string,
     helmsman: string,
     sponsors: string,
     imagePath: string,
-    flag: string,
+    flag: string
   ) {
     this.id = id;
     this.country = country;
     this.name = name;
+    this.officialName = officialName;
     this.skipper = skipper;
     this.helmsman = helmsman;
     this.sponsors = sponsors;

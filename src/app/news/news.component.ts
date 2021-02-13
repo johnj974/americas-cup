c54d236fccb995daf4116b6c07c88c7aca147418
johnj974 <jjdug@hotmail.com>
2021-02-13 15:37:40 +0000

    added covid+competition data to news page

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -10,10 +10,14 @@ import { CompetitionService } from '../services/competition.service';
   styleUrls: ['./news.component.css'],
 })
 export class NewsComponent implements OnInit {
-  covidInfo: CovidReport[] = [];
+  covidInfo = [];
   covidCountry: string;
 
   compResults = [];
+  italy = 0;
+  usa = 0;
+  uk = 0;
+  nz = 0;
 
   constructor(
     private covidService: CovidService,
@@ -38,28 +42,24 @@ export class NewsComponent implements OnInit {
     this.competitionService.retrievePosts().subscribe((posts) => {
       this.compResults = posts;
       console.log(this.compResults);
+      for (let i = 0; i < this.compResults.length; i++) {
+        if (this.compResults[i].winnerChoice === 'Italy') {
+          this.italy += 1;
+        }
+        if (this.compResults[i].winnerChoice === 'USA') {
+          this.usa += 1;
+        }
+        if (this.compResults[i].winnerChoice === 'UK') {
+          this.uk += 1;
+        }
+        if (this.compResults[i].winnerChoice === 'New Zealand') {
+          this.nz += 1;
+        }
+      }
     });
   }
 
   newsList: NewsCard[] = [
-    new NewsCard(
-      'assets/images/american-boat.jpg',
-      'head1',
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium nam .',
-      'a name'
-    ),
-    new NewsCard(
-      'assets/images/american-boat.jpg',
-      'head1',
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium nam .',
-      'a name'
-    ),
-    new NewsCard(
-      'assets/images/american-boat.jpg',
-      'head1',
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium nam .',
-      'a name'
-    ),
     new NewsCard(
       'assets/images/american-boat.jpg',
       'head1',

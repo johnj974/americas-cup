81db2f1744d4e07cbbf593cad3f378e3e647ce5c
johnj974 <jjdug@hotmail.com>
2021-02-15 19:49:17 +0000

    improved layout of news page+added content

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -71,40 +71,40 @@ export class NewsComponent implements OnInit {
 
   newsList: NewsCard[] = [
     new NewsCard(
-      'assets/images/american-boat.jpg',
-      'head1',
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium nam .',
-      'a name'
+      'assets/images/new-zealand-boat.jpg',
+      'New Zealand Progress',
+      'New Zealand progress easily, Remaining boats to fight for a place in the final. ',
+      '-Joe Harrington'
     ),
     new NewsCard(
       'assets/images/american-boat.jpg',
-      'head1',
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium nam .',
-      'a name'
+      'Patriot Capsize',
+      'The American boat Patriot capsizes, All crew members uninjured. Major repairs underway',
+      '-Kevin Cashell'
     ),
     new NewsCard(
-      'assets/images/american-boat.jpg',
-      'head1',
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium nam .',
-      'a name'
+      'assets/images/uk-boat3.jpg',
+      'UK Revival',
+      'Ineos team UK start poorly, Unable to adapt to the changing conditions, Major improvements required.',
+      '-Sounita Son'
     ),
     new NewsCard(
-      'assets/images/american-boat.jpg',
-      'head1',
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium nam .',
-      'a name'
+      'assets/images/italy2-boat.jpg',
+      'Italian Masterclass',
+      'Prada Pirelli Lunna Rosa take a commanding for nil lead in the Prada Cup final to maintain their charge for a final spot.',
+      '-Pietro Pielso'
     ),
     new NewsCard(
-      'assets/images/american-boat.jpg',
-      'head1',
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium nam .',
-      'a name'
+      'assets/images/americas-bow.jpg',
+      'Prada Cup',
+      'Ineos team Uk have it all to do after two disappointing days on  the water with Prada Pirelli Lunna Rosa opening up a commanding lead.',
+      '-Ludo Motoscafo'
     ),
     new NewsCard(
-      'assets/images/american-boat.jpg',
-      'head1',
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium nam .',
-      'a name'
+      'assets/images/new-zealand-boat2.jpg',
+      'American Cup',
+      'Who will contest the final with Emirates team New Zealand.',
+      '-Brendan Murray'
     ),
   ];
 }

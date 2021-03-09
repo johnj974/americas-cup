595a04e6ea6f1c9c59b178e6d57906cf1da3d52c
johnj974 <jjdug@hotmail.com>
2021-03-09 11:26:19 +0000

    created weatherKey

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Injectable({ providedIn: 'root' })
 export class WeatherService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private apiService: ApiService) {}
+
+  weatherKey = this.apiService.weatherKey;
 
   getWeather() {
     return this.http.get(
-      'http://api.openweathermap.org/data/2.5/weather?q=auckland&units=metric&appid=7b0fd008f9513bea6605a1407a26ec03'
+      `http://api.openweathermap.org/data/2.5/weather?q=auckland&units=metric&appid=${this.weatherKey}`
     );
   }
 }
